28e03f94a4d1f3887164e88fefc2c15485eeee08
xinxingzhao <xinxingzhao@yeah.net>
2016-12-25 18:25:32 +0800

    fix: 创建软连接

--- setup.sh
+++ setup.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 zshrc_file=~/.zshrc
-if [ -f "$zshrc_file"]; then
-    rm $zshrc_file
+vimrc_file=~/.vimrc
+tmux_file=~/.tmux.conf
+dircolors_file=~/.dircolors
+if [ -f "$zshrc_file" ]; then
+    rm -f $zshrc_file
+    echo 'remove '$zshrc_file
 fi
+if [ -f "$vimrc_file" ]; then
+    rm -f $vimrc_file
+    echo 'remove '$vimrc_file
+fi
+if [ -f "$tmux_file" ]; then
+    rm -f $tmux_file
+    echo 'remove '$tmux_file
+fi
+if [ -f "$dircolors_file" ]; then
+    rm -f $dircolors_file
+    echo 'remove '$dircolors_file
+fi
+
+ln -s ~/myconfig/zshrc ~/.zshrc
+ln -s ~/myconfig/vimrc ~/.vimrc
+ln -s ~/myconfig/tmux.conf ~/.tmux.conf
+ln -s ~/myconfig/third_party/dircolors-solarized/dircolors.ansi-light ~/.dircolors
