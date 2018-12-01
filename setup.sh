85fd5efad5ca4433608b36c3762d305222efc1dd
maxiaolong <maxiaolong@sensetime.com>
2018-12-01 17:08:39 +0800

    vundle

--- setup.sh
+++ setup.sh
@@ -45,6 +45,9 @@ cd third_party/autojump
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+# config vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # copy config
 ln -s ~/myconfig/zshrc ~/.zshrc
 ln -s ~/myconfig/vimrc ~/.vimrc
